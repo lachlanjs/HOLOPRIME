c56efae8a6dbc360e18207069374e88d1ea17c4f
lachlanjs <43869608+lachlanjs@users.noreply.github.com>
2025-04-02 06:16:32 +0000

    scipy sparse yee

--- prime_factorization.py
+++ prime_factorization.py
@@ -4,7 +4,9 @@ This file contains functions to generate a dask array of prime factorizations
 
 import numpy as np
 # import sparse
-import scipy
+import scipy as scp
+import scipy.sparse as sparse
+
 import dask.array as da
 import sympy
 
@@ -27,9 +29,13 @@ def generate_prime_factorizations(
     
     # create big sparse array
     cols = prime_count_ub(N)
+
     
-    pf_arr = sparse.COO(np.array([]), np.array([], dtype=np.bool), shape=(N, cols))
-    pf_arr = da.from_array(pf_arr, chunks=(128, cols)).map_blocks(sparse.COO)
+    
+    # pf_arr = sparse.COO(np.array([]), np.array([], dtype=np.bool), shape=(N, cols))
+    # pf_arr = da.from_array(pf_arr, chunks=(128, cols)).map_blocks(sparse.COO)
+    pf_arr = sparse.csr_array((N, cols), dtype=np.bool)
+    pf_arr = da.from_array(pf_arr, chunks=(128, cols)).map_blocks(sparse.csr_array)
 
     # get primes
     primes_N = list(sympy.primerange(N))
